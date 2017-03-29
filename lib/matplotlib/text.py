88241a2960163e6484a86680c37b06e4738a7adf
Haran Nisakaran <warmonger@wifihost-51-92-12.wireless.utsc.utoronto.ca>
2017-03-29 18:05:10 -0400

    removed erroneous spaces

--- lib/matplotlib/text.py
+++ lib/matplotlib/text.py
@@ -186,7 +186,7 @@ class Text(Artist):
     def __repr__(self):
         return "Text(%g,%g,%s)" % (self._x, self._y, repr(self._text))
 
-    def     __init__(self,
+    def __init__(self,
                  x=0, y=0, text='',
                  color=None,           # defaults to rc params
                  verticalalignment='baseline',
